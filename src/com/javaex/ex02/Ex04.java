895f92a509df1b2801a4a19baf75d19b28afd198
YOOHOJUN <YOOHOJUN@YOOHOJUN>
2021-11-15 11:47:31 +0900

    3rd

--- src/com/javaex/ex02/Ex04.java
+++ src/com/javaex/ex02/Ex04.java
@@ -21,6 +21,9 @@ public class Ex04 {
 		
 		System.out.println("당신의 이름은 "+name+" 나이는 "+age+" 입니다.");
 		
+		System.out.println("git test--04");
+		System.out.println("git test--04");
+		System.out.println("git test--04");
 		
 		sc.close();
 	}
