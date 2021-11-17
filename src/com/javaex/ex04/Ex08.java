6ab23a5f10d7906855f170a2e307e8f61506be2c
Hojun_Yoo <lisiee0@naver.com>
2021-11-17 18:04:12 +0900

    +Ex08,9

---
+++ src/com/javaex/ex04/Ex08.java
@@ -0,0 +1,20 @@
+package com.javaex.ex04;
+
+public class Ex08 {
+
+	public static void main(String[] args) {
+		
+		int i= 1;
+	
+		while(i>0) {
+		
+			i++;
+			
+			if(i%6==0&&i%14==0) {
+				System.out.println(i);
+				break;	
+			}
+
+		}
+	}
+}
