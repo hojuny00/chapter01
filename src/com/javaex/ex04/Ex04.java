f7119ee368307591e534dd5a156643b8a4aec8bb
Hojun_Yoo <lisiee0@naver.com>
2021-11-17 14:46:57 +0900

    modify

--- src/com/javaex/ex04/Ex04.java
+++ src/com/javaex/ex04/Ex04.java
@@ -4,19 +4,14 @@ public class Ex04 {
 
 	public static void main(String[] args) {
 		
-		int sum= 0;
-		int tsum= 0;
+		int sum= 0;;
 		
-		for(int a= 1; a<=10; a++ ) {
-			sum= sum+a;    //sum= sum+a
+		for(int a= 1; a<=10; a++) {
+			sum= sum+a;
 			System.out.println(a+" 까지의 합은 "+sum);
-			
-			if(a==10) {
-				tsum= sum;
-			}
-			
 		}
-		System.out.println("1부터 10까지의 정수의 합은 "+tsum+" 입니다.");
+		
+		System.out.println("1부터 10까지의 정수의 합은 "+sum+" 입니다.");
 	}
 
 }
