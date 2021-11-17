42499e0f83222929080300ec62eef1276cb2e0b7
Hojun_Yoo <lisiee0@naver.com>
2021-11-17 16:52:09 +0900

    method2 for Ex07

--- src/com/javaex/ex04/Ex07.java
+++ src/com/javaex/ex04/Ex07.java
@@ -3,8 +3,7 @@ package com.javaex.ex04;
 public class Ex07 {
 
 	public static void main(String[] args) {
-		
-		
+			
 		for(int y= 1; y<=6; y++) {	
 			for(int x= 1; x<=y; x++) {
 				System.out.print("*");
@@ -12,5 +11,21 @@ public class Ex07 {
 			
 			System.out.println();	
 		}
+		
+		
+		// method2
+		
+		/*
+		String star= "*";
+		
+		for(int y= 1; y<=6; y++) {
+			
+			System.out.print(star);
+			star= star+"*";
+			
+			System.out.println();
+		}
+		
+		*/
 	}
 }
