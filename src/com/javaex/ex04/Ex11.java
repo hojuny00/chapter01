06608a81dd85c521906f3362dae7aeef4f5f5d78
Hojun_Yoo <lisiee0@naver.com>
2021-11-18 15:18:22 +0900

    add method3

--- src/com/javaex/ex04/Ex11.java
+++ src/com/javaex/ex04/Ex11.java
@@ -5,6 +5,7 @@ public class Ex11 {
 	public static void main(String[] args) {
 		
 		// continue 사용하기
+		 
 		for(int i= 1; i<=20; i++) {
 			if(i%2==0||i%3==0) {
 				continue;
@@ -12,7 +13,7 @@ public class Ex11 {
 		System.out.println(i);	
 		} 
 		
-		// 방법2
+		// 방법2 if-else 사용하기
 		/*
 		for(int i= 1; i<=20; i++) {
 			if(i%2==0||i%3==0) {
@@ -22,6 +23,15 @@ public class Ex11 {
 				System.out.println(i);
 			}
 		}
+		*/
+		
+		// 방법3 ! 사용하기
+		
+		/* 
+		for(int i= 1; i<=20; i++)
+			if(!(i%2==0||i%3==0)) {
+				System.out.println(i);
+			}
 		*/
 	}
 }
